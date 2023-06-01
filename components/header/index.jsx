c57c5299111e871b0fb2e3d38682d3a18e47f973
duhongyu <976849662@qq.com>
2023-06-01 16:45:50 +0800

    feat：设置格式化操作

---
+++ components/header/index.jsx
@@ -0,0 +1,15 @@
+// import styles from './header.module.css'
+export default function Header(options) {
+  return (
+    <>
+      <div className="header"> Header </div>
+      <style jsx>
+        {`
+          .header {
+            height: 46px;
+          }
+        `}
+      </style>
+    </>
+  )
+}
---
+++ pages/index.jsx
@@ -0,0 +1,10 @@
+import Header from '@/components/header'
+export default function Index() {
+  return (
+    <>
+      <Header></Header>
+      index
+      <div className="header">123</div>
+    </>
+  )
+}
