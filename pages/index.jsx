fbb3e6d998387b3e24e35905576f2398b003e0e1
duhongyu <976849662@qq.com>
2023-06-01 22:04:31 +0800

    feat：完成banner图

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,21 +1,40 @@
 import Header from '@/components/header/index'
+import CarouselBanner from './index/carouselBanner'
+
 export default function Index() {
   return (
     <>
       <div className="index-container">
-        <Header></Header>
-        <img className="banner" src="imgs/headerBg2.png" alt="" />
+        <div className="header">
+          <Header></Header>
+        </div>
+        <div>
+          <img className="banner" src="imgs/headerBg2.png" alt="" />
+        </div>
+        <div className="carousel-banner">
+          <CarouselBanner />
+        </div>
       </div>
-     
+
       <style jsx>{`
         .index-container {
         }
-        .banner {
+        .header {
+          position: sticky;
+          top: 0;
+          z-index: 999;
           width: 100%;
+        }
+        .banner {
+          width: 100vw;
           height: 200px;
           position: absolute;
-          top: 0;
           left: 0;
+          top: 0;
+        }
+        .carousel-banner{
+          padding-top:184px
+
         }
       `}</style>
     </>
---
+++ pages/index/carouselBanner.jsx
@@ -0,0 +1,71 @@
+import { Carousel } from 'antd'
+const contentStyle = {
+  height: '400px',
+  background: '#364d79',
+}
+export default function CarouselBanner(options) {
+  const onChange = (currentSlide) => {
+    console.log(currentSlide)
+  }
+  return (
+    <>
+      <div className="carousel-banner">
+        <div>
+          <Carousel autoplay style={{width:"830px"}} >
+            <div  style={contentStyle}>
+              <img src="/imgs/carousel.png" alt="" />
+            </div>
+            <div  style={contentStyle}>
+              <img src="/imgs/carousel2.png" alt="" />
+            </div>
+          </Carousel>
+        </div>
+        <div className="hot">
+          <div className="hot-item">
+            <img src="/imgs/right1.png" alt="" />
+            <p>最新疫情数据</p>
+          </div>
+          <div className="hot-item">
+            <img src="/imgs/right2.png" alt="" />
+            <p>调整作息早睡早起</p>
+          </div>
+        </div>
+      </div>
+      <style jsx>{`
+        .carousel-banner {
+          height: 400px;
+          display: flex;
+          justify-content: space-between;
+        }
+        .hot {
+          display: flex;
+          flex-direction: column;
+          justify-content: space-between;
+          align-items: end;
+        }
+        .hot-item {
+          height: 193px;
+          position: relative;
+        }
+        .hot p {
+          color: #fff;
+          position: absolute;
+          left: 0;
+          right: 0;
+          bottom: 0;
+          background-color: rgba(0, 0, 0, 0.2);
+          box-shadow: 0 -5px 10px rgba(0, 0, 0, 0.2);
+          line-height: 2;
+          font-size: 14px;
+          padding: 0 10px;
+          font-weight: 700;
+          z-index: 999;
+        }
+        .hot img {
+          height: 180px;
+          width: 350px;
+        }
+      `}</style>
+    </>
+  )
+}
