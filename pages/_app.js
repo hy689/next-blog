8b97af7fb46ae6a04272ba84de58552659b0a0db
duhongyu <976849662@qq.com>
2023-06-01 15:13:19 +0800

    feat: 引入字体图标

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,9 @@
 import '@/styles/globals.css'
+import { Html } from 'next/document'
+import Head from 'next/head'
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <Component {...pageProps} />
+  )
 }
--- pages/articles/index.js
+++ pages/articles/index.js
@@ -1,9 +1,11 @@
-import Header  from "./components/header"
+import Header  from "@/pages/header";
 export default function Articles() {
   return (
     <>
       <Header />
       Articles
+      <span className="iconfont icon-aiqing1"></span>
+
     </>
   )
 }
